a1e176c567f1d244720af35fdb8ac531f2fc77c1
Alaa Shatat <alaashabaan102210@gmail.com>
2023-01-18 18:28:43 +0200

    image route

---
+++ API/src/routes/image.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+
+// controllers 
+const {Image} = require('../controllers/imageController');
+
+// routes
+router.get('/image', Image, (req, res): void=>{
+    console.log("innnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+})
+
+module.exports = router;
